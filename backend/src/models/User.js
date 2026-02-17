da9a9f0b4aaf33f794302cad6ea721ed0ba21bfc
mohamed ounissi <mohamed.ounissi@rooly.tn>
2026-02-17 08:44:59 +0100

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -68,11 +68,6 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
-  // Ensure timezone is set for existing users
-  if (!this.timezone) {
-    this.timezone = 'UTC';
-  }
-  
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
